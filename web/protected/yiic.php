6e7369c754389a2762f7deae7f10c10b093827bb
Andronik <test13r@gmail.com>
2015-04-29 15:46:12 +0500

    add YiiMailer

--- web/protected/yiic.php
+++ web/protected/yiic.php
@@ -3,5 +3,5 @@
 // change the following paths if necessary
 $yiic=dirname(__FILE__).'/../../framework/yiic.php';
 $config=dirname(__FILE__).'/config/console.php';
-
+Yii::setPathOfAlias('webroot',dirname(__FILE__).'/..');
 require_once($yiic);
